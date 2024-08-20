dc1fed265e5b50a706267089f085ca5f288ba798
Sreenandh <sreenathtp38@gmail.com>
2024-08-20 22:36:21 +0530

    sd

---
+++ lib/screens/secomdpage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class second extends StatefulWidget {
+  const second({super.key});
+
+  @override
+  State<second> createState() => _secondState();
+}
+
+class _secondState extends State<second> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+      body: Column(children: [ 
+        Text("h7666666666666666666666666666")
+      ],),
+    );
+  }
+}
